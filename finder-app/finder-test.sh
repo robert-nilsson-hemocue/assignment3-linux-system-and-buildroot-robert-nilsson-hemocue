1a8b86eabbffe460a17c461e53fa1130a6941a2a
Robert Nilsson <robert.nilsson@hemocue.se>
2026-01-02 13:35:55 +0100

    Assume commands are in PATH.

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -51,10 +51,10 @@ fi
 
 for i in $( seq 1 $NUMFILES)
 do
-	./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+	writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
-OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
+OUTPUTSTRING=$(finder.sh "$WRITEDIR" "$WRITESTR")
 echo $OUTPUTSTRING > /tmp/assignment4-result.txt
 
 # remove temporary directories
